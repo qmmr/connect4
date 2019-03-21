c3cb64707294eacaad35434d12ae92ab66b4bb41
Marcin Kumorek <kumeek@gmail.com>
2019-03-21 18:33:46 +0100

    Cleanup, add top 2 bottom, bottom 2 top :thinking:

--- matrix.py
+++ matrix.py
@@ -17,7 +17,7 @@ matrix = [
 #     ('g', 'G', 'g', 'G', 'g', 'G')
 # ]
 
-test_board = [
+b2t_board = [
     # 0    1    2    3    4    5    6
     ['-', '-', '-', '0', '1', '2', '3'],  # 0
     ['-', '-', '0', '1', '2', '3', '4'],  # 1
@@ -27,30 +27,61 @@ test_board = [
     ['2', '3', '4', '5', '-', '-', '-'],  # 5
 ]
 
-diagonals = (
+b2t_diagonals = (
     ((3, 0), (2, 1), (1, 2), (0, 3)),
     ((4, 0), (3, 1), (2, 2), (1, 3), (0, 4)),
     ((5, 0), (4, 1), (3, 2), (2, 3), (1, 4), (0, 5)),
-    ((6, 0), (5, 1), (4, 2), (3, 3), (2, 4), (1, 5)),
-    ((6, 1), (5, 2), (4, 3), (3, 4), (2, 5)),
-    ((6, 2), (5, 3), (4, 4), (3, 5)),
+    ((5, 1), (4, 2), (3, 3), (2, 4), (1, 5), (0, 6)),
+    ((5, 2), (4, 3), (3, 4), (2, 5), (1, 6)),
+    ((5, 3), (4, 4), (3, 5), (2, 6)),
 )
 
-diagonal_board = []
+t2b_board = [
+    ['3', '2', '1', '0', '-', '-', '-'],
+    ['4', '3', '2', '1', '0', '-', '-'],
+    ['5', '4', '3', '2', '1', '0', '-'],
+    ['-', '5', '4', '3', '2', '1', '0'],
+    ['-', '-', '5', '4', '3', '2', '1'],
+    ['-', '-', '-', '5', '4', '3', '2']
+]
 
-for t in diagonals:
-    # print("i: {}, t: {}".format(i, t))
+# Tuples containing (row, position) of diagonal lines, top to bottom a.k.a. t2b
+t2b_diagonals = (
+    ((2, 0), (3, 1), (4, 2), (5, 3)),
+    ((1, 0), (2, 1), (3, 2), (4, 3), (5, 4)),
+    ((0, 0), (1, 1), (2, 2), (3, 3), (4, 4), (5, 5)),
+    ((0, 1), (1, 2), (2, 3), (3, 4), (4, 5), (5, 6)),
+    ((0, 2), (1, 3), (2, 4), (3, 5), (4, 6)),
+    ((0, 3), (1, 4), (2, 5), (3, 6)),
+)
+
+
+computed_board1 = []
+
+for t in b2t_diagonals:
     new_row = []
-    for pos, row in t:
+    for row, pos in t:
         # print("row: {}, pos: {}".format(row, pos))
-        new_row.append(test_board[row][pos])
+        new_row.append(b2t_board[row][pos])
 
-    diagonal_board.append(new_row)
+    computed_board1.append(new_row)
 
 
-for l in diagonal_board:
+for l in computed_board1:
     print(l)
 
+
+computed_board2 = []
+
+for t in t2b_diagonals:
+    new_row = []
+    for row, pos in t:
+        # print("row: {}, pos: {}".format(row, pos))
+        new_row.append(t2b_board[row][pos])
+    computed_board2.append(new_row)
+
+for l in computed_board2:
+    print(l)
 # reversed_matrix = [list(val) for val in list(zip(*matrix))]
 # for l in matrix:
 # print(l)
